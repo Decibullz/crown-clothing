6b7fd4e0f3376e0d0b3c5f4f65c7f0df5302dada
Cody Snell <codyjsnell@yahoo.com>
2021-02-21 20:13:23 -0600

    creates functional component

---
+++ src/components/Preview-Collection/Preview-Collection.component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Preview-Collection.styles.scss'
+
+const CollectionPreview = () => (
+  <div className="CollectionPreview">
+    <h1 className="title"></h1>
+  </div>
+)
