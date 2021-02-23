1c69b297010ba765d0b5e503f58ba963dcd9e183
Cody Snell <codyjsnell@yahoo.com>
2021-02-23 17:21:43 -0600

    file rename

--- src/components/Preview-Collection/Preview-Collection.component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './Preview-Collection.styles.scss'
-
-const CollectionPreview = () => (
-  <div className="CollectionPreview">
-    <h1 className="title"></h1>
-  </div>
-)
