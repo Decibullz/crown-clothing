7cb07e7b3811bb7d7dabe2e55cd429033e6fff92
Cody Snell <codyjsnell@yahoo.com>
2021-02-24 20:12:25 -0600

    creates functional component

---
+++ src/pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '../../components/Sign-in/Sign-In.component'
+
+import './Sign-in-and-sign-up.styles.scss'
+
+const SignInAndSignUpPage = () => (
+  <div className="Sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SignInAndSignUpPage
