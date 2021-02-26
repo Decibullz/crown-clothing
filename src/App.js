e03c6e91866e35fc7b807595b64fb511a205af72
Cody Snell <codyjsnell@yahoo.com>
2021-02-26 16:26:08 -0600

    added user state, changed to class based component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/Homepage/HomePage.component'
 import ShopPage from './pages/Shop/ShopPage.component'
 import Header from './components/Header/Header.component'
 import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component'
+import { auth } from './firebase/firebase.util'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
