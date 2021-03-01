ba9d5a75cce0edd7beffa044d363c60d09d0920d
Cody Snell <codyjsnell@yahoo.com>
2021-02-28 23:27:14 -0600

    imports signup component

--- src/pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.js
+++ src/pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import SignIn from '../../components/Sign-in/Sign-In.component'
-
+import SignUp from '../../components/Sign-up/Sign-up.component'
 import './Sign-in-and-sign-up.styles.scss'
 
 const SignInAndSignUpPage = () => (
-  <div className="Sign-in-and-sign-up">
+  <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 )
 
