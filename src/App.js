9c8f80daa003e900d4ffd301f7435cff316c0b79
Cody Snell <codyjsnell@yahoo.com>
2021-02-28 23:29:12 -0600

    imports create prof. to set state on signup/in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
 import './App.css'
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/Homepage/HomePage.component'
 import ShopPage from './pages/Shop/ShopPage.component'
 import Header from './components/Header/Header.component'
 import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.util'
+import { auth, createUserProfileDocument } from './firebase/firebase.util'
 
 class App extends Component {
   constructor() {
@@ -19,8 +19,20 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          })
+          // console.log(this.state)
+        })
+      } else this.setState({ currentUser: userAuth })
     })
   }
 
