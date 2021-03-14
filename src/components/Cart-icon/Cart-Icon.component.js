14cc8aac901436288cd8bdd925b5c737a2e4d495
Cody Snell <codyjsnell@yahoo.com>
2021-03-13 22:38:34 -0600

    imports cart selector

--- src/components/Cart-icon/Cart-Icon.component.js
+++ src/components/Cart-icon/Cart-Icon.component.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemCount } from '../../redux/cart/cart.selectors'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './Cart-Icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 )
+// re renders every time state updates regardless if it is cart state updating. (Performance issues)
+
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   itemCount: cartItems.reduce(
+//     (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+//     0
+//   ),
+// })
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemCount(state),
+})
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
