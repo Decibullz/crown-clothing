a136b6215f2f36fe2fdd6a9f9b13d14b8f3613b8
Cody Snell <codyjsnell@yahoo.com>
2021-03-13 22:12:24 -0600

    adds dev tool extension to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,10 @@ import rootReducer from './root-reducer'
 
 const midddlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...midddlewares))
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...midddlewares) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
