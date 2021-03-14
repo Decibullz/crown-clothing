a304edb4834dae806f918059dbc36fdbc1ad85ec
Cody Snell <codyjsnell@yahoo.com>
2021-03-13 23:13:25 -0600

    change mapStateToProps to createStructuredSelector

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Header from './components/Header/Header.component'
 import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.util'
 import { setCurrentUser } from './redux/user/user.actions'
+import { createStructuredSelector } from 'reselect'
+import { selectCurrentUser } from './redux/user/user.selector'
 
 class App extends Component {
   unsubscribeFromAuth = null
@@ -55,12 +57,13 @@ class App extends Component {
     )
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+  setCurrentUser: setCurrentUser,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
