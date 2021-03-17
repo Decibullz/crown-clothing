5983af211ba61c818e9c501fe8999aed323d576e
Cody Snell <codyjsnell@yahoo.com>
2021-03-17 17:03:55 -0500

    adds toggle to hide component when btn is clicked

--- src/components/Cart-Dropdown/Cart-Dropdown.component.js
+++ src/components/Cart-Dropdown/Cart-Dropdown.component.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 import { withRouter } from 'react-router-dom'
 
 import CartItem from '../Cart-Item/Cart-Item.component'
 import CustomButton from '../Custom-Button/Custom-Button.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
-import { createStructuredSelector } from 'reselect'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './Cart-Dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -20,7 +21,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout')
+        dispatch(toggleCartHidden())
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
