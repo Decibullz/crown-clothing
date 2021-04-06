cbb1fc83d6e8601ecbd9e3bec1fb12a601c68869
Cody Snell <codyjsnell@yahoo.com>
2021-04-05 20:07:10 -0500

    integrates stripe payment button & adds test info

--- src/pages/Checkout/Checkout.component.js
+++ src/pages/Checkout/Checkout.component.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import CheckoutItem from '../../components/Cart-Dropdown/Checkout-item/Checkout-Item.component'
+import StripeCheckoutButton from '../../components/Stripe-Button/Stripe-Button.component'
 
 import {
   selectCartItems,
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${`${total}`}</span>
     </div>
+    <div className="test-warning">
+      *PLEASE USE THE FOLLOWING TEST CREDITCARD FOR PAYMENTS*
+      <br />
+      4242 4242 4242 4242 - Exp:01/24 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
