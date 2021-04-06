b3f6627567e0719e4035e8078a561c866d56fa38
Cody Snell <codyjsnell@yahoo.com>
2021-04-05 20:23:11 -0500

    wip

--- src/pages/Homepage/HomePage.component.js
+++ src/pages/Homepage/HomePage.component.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Directory from '../../components/Directory/Directory.component'
+// import Directory from '../../components/Directory/Directory.component'
 
 import './HomePage.styles.scss'
 
-const HomePage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-)
+const HomePage = () => <div className="homepage">{/* <Directory /> */}</div>
 
 export default HomePage
